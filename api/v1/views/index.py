3b894ab433d3c8e5031293619da3cef38d8139c6
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-16 13:26:29 -0500

    add stats route in the file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from flask import Flask
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def index():
     """ returns a JSON: "status": "OK" """
     return {"status": "OK"}
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """endpoint that retrieves the number of each objects by type"""
+    new_dict = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return new_dict
