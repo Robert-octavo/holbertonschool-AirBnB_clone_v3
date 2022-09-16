67b07e5a34a9bed93fa83eb6817c56d41b23a3c0
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-16 15:41:16 -0500

    [UPDATE]: Put method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -68,5 +68,5 @@ def update_by_id(state_id):
         storage.save()
         return make_response(jsonify(state.to_dict()), 200)
     except Exception:
-        return jsonify('Not a JSON'), 400
-        #abort(400, description="Not a JSON")
+        #return jsonify('Not a JSON'), 400
+        abort(400, description="Not a JSON")
